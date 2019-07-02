c9921c757637dbdd3619eb2fb66978279b726241
DarthCY <452710557@qq.com>
2019-07-02 11:00:34 +0800

    daily update 1

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@
 #define NameWidth 20	//姓名限制长度
 #define PasswordWidth 32	//密码限制长度
 #define EmailWidth 20	//邮箱限制长度
+#define MedcineNameWidth 32		//药品名称限制长度
+#define MedcineProducerNameWidth 64		//药品生产商名称限制长度
 
 int read_TimesUser = 0;
 int read_TimesMedcine = 0;
@@ -20,16 +22,21 @@ typedef struct UserInf
 	int exsit = 0;	//用户是否存在(0不存在 1存在)
 	struct UserInf* next;
 }UserLink;
+
 UserLink* head_UserLink = (UserLink*)malloc(sizeof(UserLink));
 
 typedef struct MedcineInf
 {
-	long medcine_Number;	//药品编号
-	char medcine_Name[20];	//药品名字
-	char medcine_ProducerName[10];	//生产者名字
-	int exsist = 0;	//是否存在(0不存在 1存在)
+	long medcine_Number_0;	//药品编号(单个)
+	long medcine_Number_1;	//药品编号(批次)
+	long medcine_ImportTime;	//进货时间
+	char medcine_Name[MedcineNameWidth];	//药品名字
+	char medcine_ProducerName[MedcineProducerNameWidth];	//生产者名字
+	int exsit = 0;	//是否存在(0不存在 1存在)
 	struct MedcineInf* next;
 }MedcineLink;
+
+MedcineLink* head_MedcineLink = (MedcineLink*)malloc(sizeof(MedcineLink));
 
 int UserInfReadF();
 int UserInfSaveF(UserLink* User);
@@ -39,7 +46,11 @@ void LoginF();
 void RegisterF();
 void FindPassword(UserLink* User);
 
-void UserSystem(int UserIndex);
+void UserSystem(UserLink* User);
+
+void MedcineSystem(UserLink* User);
+	void MedcineDeleAdd(UserLink* User);
+	void MedcineInquire(UserLink* User);
 
 int main()
 {
@@ -59,18 +70,23 @@ int main()
 	switch (LogOrReg)
 	{
 	case 1:
+		system("CLS");
 		LoginF();
 		break;
 	case 2:
+		system("CLS");
 		RegisterF();
 		break;
 	default:
 		printf("输入有误,请重新输入\n");
+		getchar();		//用于停顿
+		system("CLS");
 		main();
 		break;
 	}
 	return 0;
 }
+
 
 int UserInfReadF()
 {
@@ -97,7 +113,9 @@ int UserInfReadF()
 		p = q;
 	}
 	p->next = NULL;
-	free(p);
+	//free(p);
+	//free(q);
+	//free(head);		//会导致断点，原因未知
 	return 0;
 }
 
@@ -121,8 +139,12 @@ int UserInfSaveF(UserLink* User)
 			fprintf(fp, "%s     ", &q->User_Email);
 			fprintf(fp, "%d\n", q->exsit);
 		} while (doOnce != false);
-		fclose(fp);
-		return 0;
+	fclose(fp);
+	p->next = NULL;
+	//free(p);
+	//free(q);
+	//free(head);
+	return 0;
 }
 
 int UserInfSave_All_F()
@@ -148,24 +170,83 @@ int UserInfSave_All_F()
 }
 
 
+int MedcineInfReadF()
+{
+
+	MedcineLink* head = head_MedcineLink;
+	MedcineLink* q;
+	MedcineLink* p;
+	p = q = head;
+	FILE* fp = fopen("UserInf.hsp", "r");
+	if (fp == NULL)
+	{
+		printf("文件打开失败\n");
+		return 1;
+	}
+	while (!feof(fp))
+	{
+		q = (MedcineLink*)malloc(sizeof(UserLink));
+		fscanf(fp, "%ld", &p->medcine_Number_0);
+		fscanf(fp, "%ld", &p->medcine_Number_1);
+		fscanf(fp, "%ld", &p->medcine_ImportTime);
+		fscanf(fp, "%s", p->medcine_Name);
+		fscanf(fp, "%s", p->medcine_ProducerName);
+		fscanf(fp, "%d", &p->exsit);
+		p->next = q;
+		p = q;
+	}
+	p->next = NULL;
+	//free(p);
+	//free(q);
+	//free(head);		//会导致断点，原因未知
+	return 0;
+}
+
+int MedcineInfSaveF(MedcineLink* Medcine)
+{
+	MedcineLink* head = Medcine;
+	MedcineLink* p, * q;
+	p = q = head;
+	FILE* fp = fopen("MedcineInf.hsp", "a");
+	if (fp == NULL)
+	{
+		printf("文件打开失败\n");
+		return 1;
+	}
+	bool doOnce = false;
+	if (!feof(fp))
+		do {
+			fprintf(fp, "%ld     ", q->medcine_Number_0);
+			fprintf(fp, "%ld     ", q->medcine_Number_1);
+			fprintf(fp, "%ld     ", q->medcine_ImportTime);
+			fprintf(fp, "%s     ", &q->medcine_Name);
+			fprintf(fp, "%s     ", &q->medcine_ProducerName);
+			fprintf(fp, "%d\n", q->exsit);
+		} while (doOnce != false);
+		fclose(fp);
+		p->next = NULL;
+		//free(p);
+		//free(q);
+		//free(head);
+		return 0;
+}
+
+
 
 void LoginF()	//登录函数
 {
 
-	int i = 0;
-	char Input_Account[20], Input_Password[20];	//用户输入的账号和密码
+	char Input_Account[NameWidth], Input_Password[PasswordWidth];	//用户输入的账号和密码
 	printf("请输入用户名和密码\n");
 
 	printf("账号：");
 	scanf("%s", Input_Account);
-
-	UserLink* p_mov;
-	p_mov = head_UserLink;
-
+	UserLink* head = head_UserLink;
+	UserLink* p_mov = head_UserLink;
+	system("CLS");
 CheckName:
 	printf("验证中，请稍后...\n");
-	if(p_mov)
-	for (int i = 0; i < 100; i++)
+	while (p_mov)
 	{
 		if (strcmp(Input_Account, p_mov->User_Name) == 0)			//用户名正确
 		{
@@ -175,12 +256,12 @@ CheckName:
 		CheckPass:
 			if (strcmp(Input_Password, p_mov->User_Password) == 0)		//用户名正确 密码正确
 			{
-					printf("认证成功，即将进入医院信息系统\n");
-					UserSystem(i);
-					exit(0);
+				printf("认证成功，即将进入医院信息系统\n");
+				UserSystem(p_mov);
+				free(p_mov);
+				exit(0);
 			}
 			else		//用户名正确 密码错误
-
 			{
 				int yOn = 0;
 				printf("密码输入错误，请重新输入或是选择找回密码\n");
@@ -199,24 +280,26 @@ CheckName:
 				}
 			}
 		}
-		else		//用户名错误
+		else		//用户名错误(指的用户名不存在)
 		{
-			if (i == 9 && strcmp(Input_Account, p_mov->User_Name) != 0)
+			//p_mov->next = (UserLink*)malloc(sizeof(UserLink));
+			p_mov = p_mov->next;
+			if (p_mov->exsit != 1)
 			{
-				printf("此用户不存在，请重新输入用户名：");
+				printf("用户名不存在，请重新输入：");
 				scanf("%s", Input_Account);
-				goto CheckName;		//返回用户名验证
-
+				p_mov = head;
+				goto CheckName;
 			}
 		}
-		p_mov->next = (UserLink*)malloc(sizeof(UserLink));
-		p_mov = p_mov->next;
 	}
+	free(p_mov);
 }
 
 void RegisterF()
 {
-	UserLink* head = (UserLink*)malloc(sizeof(UserLink));
+	UserLink* head = head_UserLink;
+	UserLink* checkName_Link = head;
 	UserLink* p_mov = head;
 	char Input_regName[NameWidth], Input_regPassword[PasswordWidth], Input_regEmail[EmailWidth];
 	printf("请输入用户名和密码\n");
@@ -224,55 +307,65 @@ void RegisterF()
 	getchar();
 	gets_s(Input_regName, NameWidth);
 
-CheckExName:for (int i = 0; i < 100; i++)
-{
-	if (strcmp(Input_regName, p_mov->User_Name) == 0)
+	CheckExName:if (checkName_Link)
 	{
-		printf("检测到用户名与已有用户冲突，请重新输入！\n");
-		printf("用户名：");
-		scanf("%s", Input_regName);
-		goto CheckExName;
-	}
-	else
-		break;
-}
-
-			printf("请输入密码：");
-			gets_s(Input_regPassword, PasswordWidth);
-			printf("请输入邮箱：");
-			gets_s(Input_regEmail, EmailWidth);
-
-			for (int i = 0; i < 100; i++)
+		while (checkName_Link)
+		{
+			if (strcmp(Input_regName, checkName_Link->User_Name) == 0)
 			{
-				if (p_mov)
-				{
-					strcpy(p_mov->User_Name, Input_regName);
-					strcpy(p_mov->User_Password, Input_regPassword);
-					strcpy(p_mov->User_Email, Input_regEmail);
-					p_mov->exsit = 1;
-
-					if (UserInfSaveF(p_mov) == 0)
-					{
-						printf("注册成功，即将返回主菜单\n");
-						main();
-					}
-					else
-					{
-						printf("注册失败，请联系管理员\n");
-						printf("即将返回主菜单\n");
-						main();
-					}
-					exit(0);
-				}
-				else if (i == 99 && p_mov->exsit == 1)
-				{
-					printf("此系统可注册人数已满，请联系管理员增加可注册数量\n");
-					printf("注册失败，即将返回主菜单\n");
-					main();
-					exit(0);
-				}
-				p_mov = p_mov->next;
+				printf("检测到用户名与已有用户冲突，请重新输入！\n");
+				printf("用户名：");
+				fflush(stdin);
+				scanf("%s", Input_regName);
+				checkName_Link = head;
+				goto CheckExName;
 			}
+			else
+				checkName_Link = checkName_Link->next;		//移动到下一位
+		}
+		free(checkName_Link);
+	}
+
+	printf("请输入密码：");
+	scanf("%s", Input_regPassword);
+	printf("请输入邮箱：");
+	scanf("%s", Input_regEmail);
+
+	int UserNumber = 0;		//用于链表顺序计数
+	while(p_mov)
+	{
+		if (p_mov->exsit != 1)		//不存在用户
+		{
+			
+			strcpy(p_mov->User_Name, Input_regName);
+			strcpy(p_mov->User_Password, Input_regPassword);
+			strcpy(p_mov->User_Email, Input_regEmail);
+			p_mov->User_Number = UserNumber;
+			p_mov->exsit = 1;
+
+			if (UserInfSaveF(p_mov) == 0)
+			{
+				printf("注册成功，即将返回主菜单\n");
+				main();
+			}
+			else
+			{
+				printf("注册失败，请联系管理员\n");
+				printf("即将返回主菜单\n");
+				main();
+			}
+			free(p_mov);
+			exit(0);
+		}
+		else
+		{
+			UserNumber++;
+			p_mov->next = (UserLink*)malloc(sizeof(UserLink));
+			p_mov = p_mov->next;
+		}
+
+	}
+	free(p_mov);
 }
 
 void FindPassword(UserLink* User)
@@ -289,6 +382,7 @@ CheckEmail:printf("请输入注册邮箱：");
 	SpecialNumber = GetRandomCount(8);
 	for (int i = 0; i < 8; i++)
 		Str_SpecialNumber[i] = *(SpecialNumber + i);
+	printf("%s", SpecialNumber);
 	free(SpecialNumber);
 	
 	if (strcmp(Input_Email, p_mov->User_Email) == 0)
@@ -349,7 +443,163 @@ CheckEmail:printf("请输入注册邮箱：");
 
 
 
-void UserSystem(int UserIndex)
+void UserSystem(UserLink* User)
+{
+	ReInput_UserSystem:
+	int Choose = 0;
+	system("CLS");
+	printf("%s，您已进入医院信息管理系统\n\n			请选择功能模块\n\n", User->User_Name);
+	printf("1.药物信息管理\t2.病人信息管理\t3.医生信息管理\t4.退出系统\n");
+
+	scanf("%d", &Choose);
+	switch (Choose)
+	{
+		case 1:
+			printf("即将进入药物信息管理模块\n");
+			system("CLS");
+			MedcineSystem(User);
+			break;
+		case 2:
+			printf("即将进入病人信息管理系统\n");
+			system("CLS");
+			break;
+		case 3:
+			printf("即将进入医生信息管理系统\n");
+			system("CLS");
+		case 4:
+			printf("即将退出系统\n");
+			system("CLS");
+			exit(0);
+		default:
+			printf("输入有误，请重新输入\n");
+			goto ReInput_UserSystem;
+			break;
+	}
+	
+}
+
+
+
+void MedcineSystem(UserLink* User)
+{
+	int Choose = 0;
+	if (read_TimesMedcine == 0)
+	{
+		if (MedcineInfReadF() == 0)
+			read_TimesMedcine++;
+		else
+			printf("UserInf.hsp 文件信息读取失败，请联系管理员\n");
+	}
+	
+	printf("%s，您已进入药物信息管理系统\n", User->User_Name);
+	printf("1.药物信息查询\t2.药物信息修改\t3.退出系统\n");
+	printf("请输入选择的功能模块：");
+ReInput_MedcineSystem:	
+	scanf("%d", &Choose);
+
+	switch (Choose)
+	{
+		case 1:
+			printf("即将进入药物信息查询模块\n");
+			system("CLS");
+			MedcineInquire(User);
+			break;
+		case 2:
+			printf("即将进入药物信息修改模块\n");
+			system("CLS");
+			MedcineDeleAdd(User);
+			break;
+		case 3:
+			printf("即将退出系统\n");
+			system("CLS");
+			exit(0);
+		default:
+			printf("输入有误，请重新输入：");
+			goto ReInput_MedcineSystem;
+			break;
+	}
+
+}
+
+void MedcineInquire(UserLink* User)
 {
 
+}
+
+void MedcineDeleAdd(UserLink* User)
+{
+	int Choose = 0;
+	MedcineLink* head = head_MedcineLink;
+	MedcineLink* p_mov = head;
+	printf("%s，您已进入药物信息修改模块\n");
+	printf("1.添加药物信息\t2.删除药物信息\t3.修改药物信息\t4.退出系统\n");
+	printf("请选择需要的功能：");
+ReInput_MedcineDeleAdd:
+	scanf("%d", &Choose);
+
+	if (Choose == 1)	//添加药物信息
+	{
+		while (p_mov)
+		{
+		ReInput_MedcineDeleAdd_Add:
+			if (p_mov->exsit != 1)
+			{
+				printf("请输入药品编号：");
+				scanf("%ld", &p_mov->medcine_Number_0);
+				printf("请输入药品批次编号：");
+				scanf("%ld", &p_mov->medcine_Number_1);
+				printf("请输入药品采购时间：");
+				scanf("%ld", &p_mov->medcine_ImportTime);
+				printf("请输入药品名称：");
+				scanf("%s", p_mov->medcine_Name);
+				printf("请输入药品生产商名称：");
+				scanf("%s", p_mov->medcine_ProducerName);
+				p_mov->exsit = 1;
+				if (MedcineInfSaveF(p_mov) == 0)
+				{
+					int Choose_0 = 0;
+					system("CLS");
+					printf("药品信息添加成功\n");
+					printf("是否继续添加？(1.继续输入 2.返回修改模块)\n");
+					printf("请选择：");
+					scanf("%d", &Choose_0);
+					if (Choose_0 == 1)
+					{
+						goto ReInput_MedcineDeleAdd_Add;
+					}
+					else if (Choose_0 == 2)
+					{
+						printf("即将返回药品信息修改模块\n");
+						system("CLS");
+						MedcineSystem(User);
+						exit(0);
+					}
+				}
+
+			}
+			else
+			{
+				p_mov->next = (MedcineLink*)malloc(sizeof(MedcineLink));
+				p_mov = p_mov->next;
+			}
+		}
+	}
+	else if (Choose = 2)	//删除药物信息
+	{
+
+	}
+	else if (Choose = 3)	//修改药物信息
+	{
+
+	}
+	else if (Choose = 4)
+	{
+		printf("即将退出系统\n");
+		exit(0);
+	}
+	else
+	{
+		printf("输入有误，请重新输入：");
+		goto ReInput_MedcineDeleAdd;
+	}
 }
